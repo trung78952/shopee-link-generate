82f85d2ce588340bf549e2e10455d4cbb16a5570
TrungNguyen <nh11b.l99@gmail.com>
2026-05-13 13:36:00 +0700

    back link

--- functions/api/generate.js
+++ functions/api/generate.js
@@ -1,5 +1,5 @@
-// const UPSTREAM_API = 'https://hung.shpee.cc';
-const UPSTREAM_API = 'https://cuongtws.vn';
+const UPSTREAM_API = 'https://hung.shpee.cc';
+// const UPSTREAM_API = 'https://cuongtws.vn';
 
 export async function onRequestGet({ request }) {
   const { searchParams } = new URL(request.url);
